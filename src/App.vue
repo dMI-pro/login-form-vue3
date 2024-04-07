ab1c4c66aeded51b0672ff732a9ff3f8b97749fe
Dmitriy <sorktt@mail.ru>
2024-04-07 21:21:49 +0300

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,72 @@
 <template>
+<form class="login-form">
+    <BaseInput
+      v-model="parentModelEmail"
+      label="Enter your email"
+      placeholder="Placeholder" />
+
+  <div class="login-form__social">
+    <BaseInput
+      v-model="parentModelMessenger"
+      label="Messenger" 
+      placeholder="Placeholder"
+      class="login-form__messanger" />
+
+      <BaseInput
+      v-model="parentModelNickname"
+      label="Nickname" 
+      placeholder="Placeholder"
+      class="login-form__nickname" />
+  </div>
+
   <BaseInput
     v-model="parentModelPassword"
-    label="Enter password" 
-    placeholder="Placeholder"
-    :classList="['']"
-    hint="Pleaes enter wathout mistakes" />
+    label="Enter password"
+    placeholder="Placeholder" />
+
+    <BaseInput
+    v-model="parentModelConfirmPassword"
+    label="Confirm the password"
+    placeholder="Placeholder" />
+</form>
 </template>
 
 <script setup>
 import BaseInput from '@components/BaseInput.vue'
 import { ref } from 'vue'
 
+const parentModelEmail = ref('');
 const parentModelPassword = ref('');
 
 </script>
 
+<style lang="scss">
+@import '@scss/_variables.scss';
+
+body {
+  background-color: #f5f5f5;
+}
+#app {
+  font-family: 'Rubik';
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+}
+.login-form {
+  min-width: 343px;
+  color: $mainColor;
+
+  &__social {
+    display: flex;
+    justify-content: space-between;
+    gap: 16px;
+  }
+
+  &__messanger {
+    flex: 0 0 20%;
+  }
+  &__nickname {
+    flex: 0 0 75%;
+  }
+}
+</style>
--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,66 +1,69 @@
 <template>
-    <div class="base-input">
+    <div class="base-input" >
         <div class="base-input__label">
             {{ props.label }}
         </div>
         <input
             :placeholder="props.placeholder"
             :value="props.modelValue"
-            :class="['base-input__input', ...props.classList] "
+            class="base-input__input"
             @input="handlerInput" />
-        <div class="base-input__hint">
-            {{ props.hint }}
-        </div>
     </div>
 </template>
 
 <script setup>
-const props = defineProps({
-    modelValue: {
-        type: String,
-        required: true,
-    },
-    classList: {
-        type: Array,
-        default: [],
-    },
-    label: {
-        type: String,
-        default: 'Label input'
-    },
-    placeholder: {
-        type: String,
-        default: 'Input placeholder'
-    },
-    hint: {
-        type: String,
-        default: 'Hint'
-    },
-})
+    const props = defineProps({
+        modelValue: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            default: 'Label input'
+        },
+        placeholder: {
+            type: String,
+            default: 'Input placeholder'
+        },
+    })
 
-const emit = defineEmits(['update:modelValue']);
+    const emit = defineEmits(['update:modelValue']);
 
-const handlerInput = (event) => {
-    emit('update:modelValue', event.target.value)
-}
+    const handlerInput = (event) => {
+        emit('update:modelValue', event.target.value)
+    }
 </script>
 
 <style lang="scss" scoped>
      @import '@scss/_variables.scss';
     
     .base-input {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        margin-bottom: 16px;
+
         &__label {
-            color: #838A98;
-            font-family: 'Rubik';
+            margin-bottom: 4px;
+            padding: 4px 0;
             font-size: 14px;
             line-height: 16px;
         }
         &__input {
-            border: 2px solid $borderColor;
-        }
-        &__hint {
-            border: 2px solid $borderColor;
+            background-color: $primaryColor;
+            border: 1px solid $borderColorDefault;
+            border-radius: $borderRadius;
+            margin-bottom: 4px;
+            padding: 12px 16px;
+            width: 100%;
+            box-sizing: border-box;
+
+            &::placeholder {
+                font-family: 'Rubik';
+                font-size: 16px;
+                color: $placeholderColor;
+            }
         }
     }
-    
 </style>
