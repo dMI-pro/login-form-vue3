43e65c45d6bbdffbb9eb3a99615f66737d952ba3
Dmitriy <sorktt@mail.ru>
2024-04-07 16:10:52 +0300

    base input

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <BaseInput
+    v-model="parentModelPassword"
+    label="Enter password" 
+    placeholder="Placeholder"
+    :classList="['']"
+    hint="Pleaes enter wathout mistakes" />
+</template>
+
+<script setup>
+import BaseInput from '@components/BaseInput.vue'
+import { ref } from 'vue'
+
+const parentModelPassword = ref('');
+
+</script>
+
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="base-input">
+        <div class="base-input__label">
+            {{ props.label }}
+        </div>
+        <input
+            :placeholder="props.placeholder"
+            :value="props.modelValue"
+            :class="['base-input__input', ...props.classList] "
+            @input="handlerInput" />
+        <div class="base-input__hint">
+            {{ props.hint }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    modelValue: {
+        type: String,
+        required: true,
+    },
+    classList: {
+        type: Array,
+        default: [],
+    },
+    label: {
+        type: String,
+        default: 'Label input'
+    },
+    placeholder: {
+        type: String,
+        default: 'Input placeholder'
+    },
+    hint: {
+        type: String,
+        default: 'Hint'
+    },
+})
+
+const emit = defineEmits(['update:modelValue']);
+
+const handlerInput = (event) => {
+    emit('update:modelValue', event.target.value)
+}
+</script>
+
+<style lang="scss" scoped>
+     @import '@scss/_variables.scss';
+    
+    .base-input {
+        &__label {
+            color: #838A98;
+            font-family: 'Rubik';
+            font-size: 14px;
+            line-height: 16px;
+        }
+        &__input {
+            border: 2px solid $borderColor;
+        }
+        &__hint {
+            border: 2px solid $borderColor;
+        }
+    }
+    
+</style>
